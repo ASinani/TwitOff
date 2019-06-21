30b5e3bc6484bab85af49e881f5b156659532ad5
TheJoys2019 <artin.sinani@gmail.com>
2019-06-20 22:43:33 -0700

    Updated files

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL') #sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV') #'debug' # TODO change before deploying
+    DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Hello Twitoff!'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.rooute('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
     return app
+    
